649e85dc1ae0d9149387f7ab9d32d6988a7151a9
J. E. Davies <james.davies@ibm.com>
2019-02-19 14:11:19 +0000

    Framework Property File Class (#66)
    * Pushing an update to git
    
    * Pushing an update to git
    
    * FrameworkPropertyFile Changes and re-design
    
    * More unit tests for watching
    
    * Watch tests working, and correcting pointer issues with properties assignment
    
    * FPF ready for review
    
    * Pushing changes to test on another platform
    
    * Refactored - watcher code changed method
    
    * Refactored - watcher code changed method
    
    * Push to show bugs
    
    * Pushing with sync added to methods
    
    * Chnaged, still broke
    
    * FPF with 90+ code coverage
    
    * Pushing to fork for backup
    
    * Cleaning up code smells
    
    * replaced thread.sleep
    
    * Added some comments
    
    * Cleaned some smells
    
    * Changed for review comments and added javadoc

---
+++ ejat-parent/ejat-framework/src/main/java/io/ejat/framework/spi/FrameworkPropertyFileException.java
@@ -0,0 +1,26 @@
+package io.ejat.framework.spi;
+
+public class FrameworkPropertyFileException extends Exception {
+	private static final long serialVersionUID = 2L;
+
+	public FrameworkPropertyFileException() {
+	}
+
+	public FrameworkPropertyFileException(String message) {
+		super(message);
+	}
+
+	public FrameworkPropertyFileException(Throwable cause) {
+		super(cause);
+	}
+
+	public FrameworkPropertyFileException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public FrameworkPropertyFileException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
