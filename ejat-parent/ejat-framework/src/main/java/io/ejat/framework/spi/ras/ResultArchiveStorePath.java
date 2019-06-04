3bf3269dc7a501096b8aa2085785d550b1ca738e
Michael Baylis <Michael.Baylis@uk.ibm.com>
2019-06-04 17:48:48 +0100

    SQ change

--- ejat-parent/ejat-framework/src/main/java/io/ejat/framework/spi/ras/ResultArchiveStorePath.java
+++ ejat-parent/ejat-framework/src/main/java/io/ejat/framework/spi/ras/ResultArchiveStorePath.java
@@ -576,6 +576,9 @@ public class ResultArchiveStorePath implements Path {
     
     @Override
     public boolean equals(Object o) {
+    	if (o == null) {
+    		return false;
+    	}
     	return toString().equals(o.toString());
     }
 
