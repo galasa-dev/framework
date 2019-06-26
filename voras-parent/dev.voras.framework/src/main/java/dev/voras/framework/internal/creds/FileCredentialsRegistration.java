2de3b2477915c0b7b4dc0c2e62976d367ba093ab
Michael Baylis <Michael.Baylis@uk.ibm.com>
2019-06-26 13:46:50 +0100

    Correct the file creds registration

--- voras-parent/dev.voras.framework/src/main/java/dev/voras/framework/internal/creds/FileCredentialsRegistration.java
+++ voras-parent/dev.voras.framework/src/main/java/dev/voras/framework/internal/creds/FileCredentialsRegistration.java
@@ -18,21 +18,24 @@ import dev.voras.framework.spi.creds.ICredentialsStoreRegistration;
 @Component(service= {ICredentialsStoreRegistration.class})
 public class FileCredentialsRegistration implements ICredentialsStoreRegistration {
 
-    /**
+	/**
 	 * <p>This method registers this as the only Creds file.</p>
 	 * 
 	 * @param IFrameworkInitialisation
 	 * @throws CredentialsStoreException
 	 */
-    @Override
-    public void initialise(@NotNull IFrameworkInitialisation frameworkInitialisation) throws CredentialsException {
-        try {
-            URI creds = frameworkInitialisation.getCredentialsStoreUri();
-            FileCredentialsStore fcs = new FileCredentialsStore(creds, frameworkInitialisation.getFramework());
-            frameworkInitialisation.registerCredentialsStore(fcs);
-        } catch (Exception e ) {
-            throw new CredentialsException("Could not initialise Framework Property File CREDs", e);
-        }
-    }
+	@Override
+	public void initialise(@NotNull IFrameworkInitialisation frameworkInitialisation) throws CredentialsException {
+		try {
+			URI creds = frameworkInitialisation.getCredentialsStoreUri();
+
+			if (creds.getScheme().equals("file")) {
+				FileCredentialsStore fcs = new FileCredentialsStore(creds, frameworkInitialisation.getFramework());
+				frameworkInitialisation.registerCredentialsStore(fcs);
+			}
+		} catch (Exception e ) {
+			throw new CredentialsException("Could not initialise Framework Property File CREDs", e);
+		}
+	}
 
 }
