f5e1c725da47db059c139924751ee1f0dfba5625
Michael Baylis <Michael.Baylis@uk.ibm.com>
2019-08-27 11:06:31 +0100

    New Test Catalog api server

---
+++ galasa-parent/dev.galasa.framework.api.testcatalog/src/main/java/dev/galasa/framework/api/testcatlog/internal/TestcatalogStreams.java
@@ -0,0 +1,188 @@
+package dev.galasa.framework.api.testcatlog.internal;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.ServiceScope;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+/**
+ * Basic Test Catalog store
+ * 
+ * @author Michael Baylis
+ *
+ */
+@Component(
+		service=Servlet.class,
+		scope=ServiceScope.PROTOTYPE,
+		property= {"osgi.http.whiteboard.servlet.pattern=/testcatalog/*"},
+		configurationPid= {"dev.galasa"},
+		configurationPolicy=ConfigurationPolicy.OPTIONAL,
+		name="Galasa Test Catalog Streams"
+		)
+public class TestcatalogStreams extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static final Log logger = LogFactory.getLog(TestcatalogStreams.class);
+	
+	private final Pattern patternValidStreamName = Pattern.compile("/[a-z0-9-_]+");
+	
+	private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+	private Path catalogDirectory;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		checkDirectory();
+
+		String extraPath = req.getPathInfo();
+		if (!checkPath(resp, extraPath)) {
+			return;
+		}
+		
+		String streamName = extraPath.substring(1);
+		
+		String contentType = req.getHeader("Accept");
+		if (!"application/json".equals(contentType)) {
+			resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "only application/json supported");
+			return;
+		}
+		
+		Path actualFile = catalogDirectory.resolve(streamName);
+		
+		if (!Files.exists(actualFile)) {
+			resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Test Catalog is missing");
+			return;
+		}
+		
+		resp.setContentType("application/json");
+		resp.setContentLengthLong(Files.size(actualFile));
+		
+		Files.copy(actualFile, resp.getOutputStream());
+
+		resp.setStatus(200);
+	}
+
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		checkDirectory();
+		
+		String extraPath = req.getPathInfo();
+		if (!checkPath(resp, extraPath)) {
+			return;
+		}
+		
+		String streamName = extraPath.substring(1);
+		
+		String contentType = req.getHeader("Content-Type");
+		if (!"application/json".equals(contentType)) {
+			resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "only application/json supported");
+			return;
+		}
+
+		//*** Read it in just to make sure it looks ok
+		//*** TODO need to check the length or send to disk or something to avoid DOS
+		
+		String jsonData = IOUtils.toString(req.getReader());
+		JsonObject tc = gson.fromJson(jsonData, JsonObject.class);
+		
+		if (tc == null) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Test Catalog content is missing");
+			return;
+		}
+		
+		if (!tc.has("classes") 
+				|| !tc.has("bundles") 
+				|| !tc.has("packages")) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid Test Catalog");
+			return;
+		}
+
+		Path actualFile = catalogDirectory.resolve(streamName);
+		
+		Files.write(actualFile, jsonData.getBytes("utf-8"), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+		
+		logger.info("Test Catalog written for stream " + streamName);
+				
+		resp.setStatus(200);
+	}
+
+
+	private boolean checkPath(HttpServletResponse resp, String path) throws IOException {
+		Matcher m = patternValidStreamName.matcher(path);
+		if (!m.matches()) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid stream name '" + path + "', must match regex '" + patternValidStreamName.pattern() + "'");
+			return false;
+		}
+		return true;
+	}
+
+
+	private void checkDirectory() throws IOException {
+		synchronized (getClass()) {
+			if (catalogDirectory == null) {
+				catalogDirectory = Paths.get(System.getProperty("karaf.data")).resolve("galasa").resolve("testcatalogs");
+			}
+			if (!Files.exists(catalogDirectory)) {
+				Files.createDirectories(catalogDirectory);
+			}
+		}
+	}
+
+
+	@Activate
+	void activate(Map<String, Object> properties) {
+		modified(properties);
+	}
+
+	@Modified
+	void modified(Map<String, Object> properties) {
+		Object oDirectoryProperty = properties.get("framework.testcatalog.directory");
+		if (oDirectoryProperty != null && oDirectoryProperty instanceof String) {
+			String directoryProperty = (String) oDirectoryProperty;
+			try {
+				catalogDirectory = Paths.get(new URL(directoryProperty).toURI());
+				logger.info("Catalog directorty set to " + catalogDirectory.toUri().toString());
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			catalogDirectory = null;
+		}
+	}
+
+	@Deactivate
+	void deactivate() {
+		this.catalogDirectory = null;
+	}
+
+}
---
+++ galasa-parent/dev.galasa.framework.api.testcatalog/src/main/java/dev/galasa/framework/api/testcatlog/internal/Testcatalogs.java
@@ -0,0 +1,202 @@
+package dev.galasa.framework.api.testcatlog.internal;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.ServiceScope;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+
+/**
+ * Basic Test Catalog store
+ * 
+ * @author Michael Baylis
+ *
+ */
+@Component(
+		service=Servlet.class,
+		scope=ServiceScope.PROTOTYPE,
+		property= {"osgi.http.whiteboard.servlet.pattern=/testcatalog"},
+		configurationPid= {"dev.galasa"},
+		configurationPolicy=ConfigurationPolicy.OPTIONAL,
+		name="Galasa Test Catalogs"
+		)
+public class Testcatalogs extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static final Log logger = LogFactory.getLog(Testcatalogs.class);
+
+	private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+	private Path catalogDirectory;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		checkDirectory();
+
+		//*** locate the cached list and obtain the modified date
+		boolean rebuildCache = false;
+		FileTime cacheLastModified = FileTime.fromMillis(0);
+		Path cachePath = catalogDirectory.resolve("cache.json");
+		if (Files.exists(cachePath)) {
+			cacheLastModified = Files.getLastModifiedTime(cachePath);
+		} else {
+			rebuildCache = true;
+		}
+
+
+		//*** Get a list of the streams and newest date
+		CatalogConsumer consumer = new CatalogConsumer(cacheLastModified);
+		Files.list(catalogDirectory).forEach(consumer);
+
+		List<Path> catalogs = consumer.getCatalogs();
+
+		JsonObject jsonCache = null;
+		if (rebuildCache || consumer.rebuildCache()) {
+			jsonCache = rebuildCacheFile(catalogs, cachePath);
+		} else {
+			jsonCache = gson.fromJson(Files.newBufferedReader(cachePath), JsonObject.class);
+		}
+
+		String jsonResponse = gson.toJson(jsonCache);
+		
+		resp.setContentType("application/json");
+		resp.setContentLengthLong(jsonResponse.length());
+		IOUtils.write(jsonResponse, resp.getOutputStream(), "utf-8");
+		
+		
+		
+		resp.setStatus(200);
+	}
+
+
+	private JsonObject rebuildCacheFile(List<Path> catalogs, Path cachePath) throws JsonSyntaxException, JsonIOException, IOException {
+		JsonObject jsonCache = new JsonObject();
+		JsonObject  jsonCatalogs = new JsonObject();
+		jsonCache.add("catalogs", jsonCatalogs);
+
+
+		for(Path pathCatalog : catalogs) {
+			JsonObject jsonCatalog = gson.fromJson(Files.newBufferedReader(pathCatalog), JsonObject.class);
+
+			if (jsonCatalog != null) {
+				JsonObject jsonCacheCatalog = new JsonObject();
+				jsonCatalogs.add(pathCatalog.getFileName().toString(), jsonCacheCatalog);
+
+				jsonCacheCatalog.add("name", jsonCatalog.getAsJsonObject("name"));
+				jsonCacheCatalog.add("built", jsonCatalog.getAsJsonObject("build"));
+			}
+		}
+
+		String data = gson.toJson(jsonCache);
+		Files.write(cachePath, data.getBytes("utf-8"));
+
+		return jsonCache;
+	}
+
+
+	private void checkDirectory() throws IOException {
+		synchronized (getClass()) {
+			if (catalogDirectory == null) {
+				catalogDirectory = Paths.get(System.getProperty("karaf.data")).resolve("galasa").resolve("testcatalogs");
+			}
+			if (!Files.exists(catalogDirectory)) {
+				Files.createDirectories(catalogDirectory);
+			}
+		}
+	}
+
+
+	@Activate
+	void activate(Map<String, Object> properties) {
+		modified(properties);
+	}
+
+	@Modified
+	void modified(Map<String, Object> properties) {
+		Object oDirectoryProperty = properties.get("framework.testcatalog.directory");
+		if (oDirectoryProperty != null && oDirectoryProperty instanceof String) {
+			String directoryProperty = (String) oDirectoryProperty;
+			try {
+				catalogDirectory = Paths.get(new URL(directoryProperty).toURI());
+				logger.info("Catalog directorty set to " + catalogDirectory.toUri().toString());
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			catalogDirectory = null;
+		}
+	}
+
+	@Deactivate
+	void deactivate() {
+		this.catalogDirectory = null;
+	}
+
+	private static final class CatalogConsumer implements Consumer<Path> {
+
+		private ArrayList<Path> catalogs = new ArrayList<>();
+		private FileTime cacheLastModified;
+		private boolean rebuildCache = false;
+
+		private CatalogConsumer(FileTime cacheLastModified) {
+			this.cacheLastModified = cacheLastModified;
+		}
+
+		@Override
+		public void accept(Path t) {
+			if (Files.isRegularFile(t) && !t.getFileName().toString().equals("cache.json")) {
+				catalogs.add(t);
+
+				try {
+					FileTime catalogtime = Files.getLastModifiedTime(t);
+
+					if (catalogtime.compareTo(cacheLastModified) >= 0) {
+						rebuildCache = true;
+					}
+				} catch (IOException e) {
+					rebuildCache = true;
+					e.printStackTrace();
+				}
+			}
+		}
+
+		private boolean rebuildCache() {
+			return this.rebuildCache;
+		}
+
+		public List<Path> getCatalogs() {
+			return this.catalogs;
+		}
+
+	}
+
+}
