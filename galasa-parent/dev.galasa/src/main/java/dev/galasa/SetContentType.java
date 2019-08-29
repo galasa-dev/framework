f93ef52b94ba768d011be9bff1e1dd7ed722b585
Michael Baylis <Michael.Baylis@uk.ibm.com>
2019-08-29 17:08:13 +0100

    Allow an openoption to set the contenttype

---
+++ galasa-parent/dev.galasa/src/main/java/dev/galasa/SetContentType.java
@@ -0,0 +1,17 @@
+package dev.galasa;
+
+import java.nio.file.OpenOption;
+
+public class SetContentType implements OpenOption {
+	
+	private final ResultArchiveStoreContentType contentType;
+	
+	public SetContentType(ResultArchiveStoreContentType contentType) {
+		this.contentType = contentType;
+	}
+	
+	public ResultArchiveStoreContentType getContentType() {
+		return contentType;
+	}
+	
+}
