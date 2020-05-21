2e240e895e66f691904fc6292c7e25ad669cc887
Richard Somers <rsomers1@sheffield.ac.uk>
2020-05-21 13:14:21 +0100

    Clean up commit
    Signed-off-by: Richard Somers <rsomers1@sheffield.ac.uk>

--- galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/internal/cps/FpfConfigurationPropertyStore.java
+++ galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/internal/cps/FpfConfigurationPropertyStore.java
@@ -5,10 +5,7 @@
  */
 package dev.galasa.framework.internal.cps;
 
-import java.io.IOException;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
