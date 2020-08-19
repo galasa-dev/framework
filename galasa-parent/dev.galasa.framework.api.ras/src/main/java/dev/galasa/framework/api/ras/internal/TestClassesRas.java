91966d54443ec5b3d7383b9f5a6c4a23652895b5
Diana Slepikaite <d.slepikaite@gmail.com>
2020-08-19 14:03:39 +0100

    Deleted sorting by bundle
    Signed-off-by: Diana Slepikaite <d.slepikaite@gmail.com>

--- galasa-parent/dev.galasa.framework.api.ras/src/main/java/dev/galasa/framework/api/ras/internal/TestClassesRas.java
+++ galasa-parent/dev.galasa.framework.api.ras/src/main/java/dev/galasa/framework/api/ras/internal/TestClassesRas.java
@@ -35,7 +35,7 @@ public class TestClassesRas extends HttpServlet {
      */
     private static final long serialVersionUID = 1L;
 
-    //private final List<String> DUMMYQUERY = Arrays.asList("assc","ascc","aasc","ascbundle","descc","ddesc","deesc", "desc" );
+
     private final List<RasTestClass> TESTCLASSDUMMY = Arrays.asList(new RasTestClass("FirstTest", "Abundle"),new RasTestClass("SecondTest", "BigBundle"),new RasTestClass("ThirdTest", "MiniBundle"),new RasTestClass("ZeroTest", "NanoBundle"));
     
 
@@ -50,27 +50,7 @@ public class TestClassesRas extends HttpServlet {
     		TESTCLASSDUMMY.sort(Comparator.comparing(RasTestClass::getTestClass));
     	}if(!ExtractQuerySort.isAscending(query, "testclass")) {
     		TESTCLASSDUMMY.sort(Comparator.comparing(RasTestClass::getTestClass).reversed());
-    	}if(ExtractQuerySort.isAscending(query, "bundle")) {
-    		TESTCLASSDUMMY.sort(Comparator.comparing(RasTestClass::getBundleName));
-    	}if(!ExtractQuerySort.isAscending(query, "bundle")) {
-    		TESTCLASSDUMMY.sort(Comparator.comparing(RasTestClass::getBundleName).reversed());
     	}
-
-        // for(String search: DUMMYQUERY){
-        //         if(search.equals("asctest")){
-        //             TESTCLASSDUMMY.sort(Comparator.comparing(RasTestClass::getTestClass));
-        //         }
-        //         else if(search.equals("desctest")){
-        //             TESTCLASSDUMMY.sort(Comparator.comparing(RasTestClass::getTestClass).reversed());
-        //         }
-        //         else if(search.equals("ascbundle")){
-        //             TESTCLASSDUMMY.sort(Comparator.comparing(RasTestClass::getBundleName));
-        //         }
-        //         else if(search.equals("descbundle")){
-        //             TESTCLASSDUMMY.sort(Comparator.comparing(RasTestClass::getBundleName).reversed());
-        //         }
-        //     }
-        
             JsonElement json = new Gson().toJsonTree(TESTCLASSDUMMY);
             JsonObject testclasses = new JsonObject();
             testclasses.add("testclasses", json);
