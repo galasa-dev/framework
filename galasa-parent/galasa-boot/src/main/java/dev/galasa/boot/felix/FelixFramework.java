64faca4862771485731e5c860340c92bedbefa4e
Matthew Chivers <matthewchivers@outlook.com>
2020-10-12 17:29:28 +0100

    Added FelixFramework method runRestoreCPS
    Signed-off-by: Matthew Chivers <matthewchivers@outlook.com>

--- galasa-parent/galasa-boot/src/main/java/dev/galasa/boot/felix/FelixFramework.java
+++ galasa-parent/galasa-boot/src/main/java/dev/galasa/boot/felix/FelixFramework.java
@@ -487,7 +487,59 @@ public class FelixFramework {
 
     }
     
-    
+    /**
+     * Restore the CPS Properties
+     * 
+     * @param boostrapProperties  the bootstrap properties
+     * @param overridesProperties the override properties
+     * @param filePath 
+     * @throws LauncherException
+     */
+    public void runRestoreCPS(Properties boostrapProperties, Properties overridesProperties, String filePath) throws LauncherException {
+
+        // Get the framework bundle
+        Bundle frameWorkBundle = getBundle("dev.galasa.framework");
+        
+        String className = "RestoreCPS";
+        String methodName = "restore";
+        
+        // Get the dev.galasa.framework.RestoreCPS class service
+        String classString = "dev.galasa.framework." + className;
+        String filterString = "(" + Constants.OBJECTCLASS + "=" + classString + ")";
+        
+        ServiceReference<?>[] serviceReferences;
+        try {
+            serviceReferences = frameWorkBundle.getBundleContext().getServiceReferences(classString, filterString);
+        } catch (InvalidSyntaxException e) {
+            throw new LauncherException("Unable to get framework service reference", e);
+        }
+        if (serviceReferences == null || serviceReferences.length != 1) {
+            throw new LauncherException("Unable to get single reference to " + className + " service: "
+                    + ((serviceReferences == null) ? 0 : serviceReferences.length) + " service(s) returned");
+        }
+        
+        Object service = frameWorkBundle.getBundleContext().getService(serviceReferences[0]);
+        if (service == null) {
+            throw new LauncherException("Unable to get " + className + " service");
+        }
+
+        // Get the dev.galasa.framework.RestoreCPS#restore() method
+        Method runBackupCPSMethod;
+        try {
+            runBackupCPSMethod = service.getClass().getMethod(methodName, Properties.class, Properties.class, String.class);
+        } catch (NoSuchMethodException | SecurityException e) {
+            throw new LauncherException("Unable to get Framework " + className + " " + methodName + " method", e);
+        }
+        
+        // Invoke the runBackupCPSMethod method
+        logger.debug("Invoking " + className + " " + methodName + "()");
+        try {
+            runBackupCPSMethod.invoke(service, boostrapProperties, overridesProperties, filePath);
+        } catch (InvocationTargetException | IllegalAccessException | IllegalArgumentException e) {
+            throw new LauncherException(e.getCause());
+        }
+        
+    }
     
     public void runWebApiServer(Properties boostrapProperties, Properties overridesProperties, List<String> bundles,
             Integer metrics, Integer health) throws LauncherException  {
@@ -536,8 +588,6 @@ public class FelixFramework {
             throw new LauncherException(e.getCause());
         }
     }
-
-
 
     /**
      * Run the Kubernetes Controller Server
