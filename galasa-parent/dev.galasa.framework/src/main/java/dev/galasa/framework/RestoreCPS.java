1a9da5d8fd7ce651a4a4f408737b5d9d55ec3e49
Matthew Chivers <matthewchivers@outlook.com>
2020-10-19 17:31:22 +0100

    Loads in properties and outputs restoration changes/status to log.
    Signed-off-by: Matthew Chivers <matthewchivers@outlook.com>

--- galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/RestoreCPS.java
+++ galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/RestoreCPS.java
@@ -12,11 +12,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -35,6 +38,9 @@ public class RestoreCPS {
     private IFramework      framework;
     
     private Path            path;
+    
+    private Map<String, IConfigurationPropertyStoreService>     namespaceCPS 
+                            = new HashMap<String ,IConfigurationPropertyStoreService>();
     
     /**
      * <p>Restores CPS properties from a specified file</p>
@@ -58,8 +64,11 @@ public class RestoreCPS {
         framework = frameworkInitialisation.getFramework();
         
         Properties properties = getProperties(filePath);
-        outputCPS(properties);
-        
+        if (properties != null) {
+            restoreProperties(properties);
+        } else {
+            logger.info("No properties found to restore.");
+        }
     }
     
     private Properties getProperties(String filePath) {
@@ -87,23 +96,151 @@ public class RestoreCPS {
 
     }
     
-    private void outputCPS(Properties properties) throws ConfigurationPropertyStoreException {
+    private void restoreProperties(Properties properties) throws ConfigurationPropertyStoreException {
         
-        Map<String, IConfigurationPropertyStoreService> namespaceCPS = new HashMap<String ,IConfigurationPropertyStoreService>();
+        logger.info(String.format("| %-10s | %-15s | %-30s | %-15s | %-15s |", "STATUS", "NAMESPACE", "PROPERTY", "OLD VALUE", "NEW VALUE"));
+        logger.info(String.format("|%-12s|%-17s|%-32s|%-17s|%-17s|", getDashes(12), getDashes(17), getDashes(32), getDashes(17), getDashes(17)));
+        
+        Map<String, String> invalidProperties = new HashMap<String, String>();
         
         for (Entry<Object, Object> prop : properties.entrySet()) {
-            String key = prop.getKey().toString();
-            String value = prop.getValue().toString();
-            String[] kvp = key.split("\\.", 2);
-            String namespace = kvp[0];
-            String property = kvp[1];
-            
-            logger.debug("Namespace: " + namespace + " Property: " + property);
-            if (!namespaceCPS.containsKey(namespace)) {
-                logger.debug("Creating CPS from Namespace: " + namespace);
-                namespaceCPS.put(namespace, framework.getConfigurationPropertyService(namespace));
+            if (isValidProperty(prop.getKey().toString())) {
+                restoreProperty(prop);
+            } else {
+                invalidProperties.put(prop.getKey().toString(), prop.getValue().toString());
             }
-            namespaceCPS.get(namespace).setProperty(property, value);
         }
+        logger.info(String.format("|%-12s|%-17s|%-32s|%-17s|%-17s|", getDashes(12), getDashes(17), getDashes(32), getDashes(17), getDashes(17)));
+
+        if (invalidProperties.size() > 0) {
+            logger.info("Could not restore the following due to malformed property name:");
+            for (Entry<String, String> prop : invalidProperties.entrySet()) {
+                logger.info("NAME: " + prop.getKey().toString() + "\tVALUE: " + prop.getValue().toString());
+            }
+        }
+        
+    }
+    
+    
+    /**
+     * <p>Splits a string into two parts: a prefix and a suffix.</p>
+     * <p>Prefix: Anything before the first dot "."</p>
+     * <p>Suffix: Anything after the first dot "."</p>
+     * 
+     * <p>Position specified as 0 or 1
+     * 
+     * @param str
+     * @param position
+     * @return dashes
+     */
+    private void restoreProperty(Entry<Object, Object> prop) throws ConfigurationPropertyStoreException {
+        
+        String namespace = getPropertyPrefix(prop.getKey().toString());
+        String property = getPropertySuffix(prop.getKey().toString());
+        
+        String newValue = prop.getValue().toString();
+        String currentValue = "";
+        
+        String propertyRestoreStatus = "";
+        
+        if (!namespaceCPS.containsKey(namespace)) {
+            // Create CPS instance if it doesn't yet exist
+            namespaceCPS.put(namespace, framework.getConfigurationPropertyService(namespace));
+        }
+        
+        currentValue = getExistingValue(namespace, property);
+        namespaceCPS.get(namespace).setProperty(property, newValue);
+        
+        if (currentValue != null) {
+            if (currentValue.equals(newValue)) {
+                propertyRestoreStatus = "NO CHANGE";
+            } else {
+                propertyRestoreStatus = "UPDATED";
+            }
+        } else {
+            propertyRestoreStatus = "CREATED";
+        }
+        
+        logger.info(String.format("| %-10s | %-15s | %-30s | %-15s | %-15s |", propertyRestoreStatus, namespace, property, currentValue, newValue));
+        
+    }
+    
+    /**
+     * <p><Retrieves Property Prefix</p>
+     * 
+     * @param propertyName
+     * @return dashes
+     */
+    private String getPropertyPrefix(String propertyName) throws ConfigurationPropertyStoreException {
+        return propSplit(propertyName, 0);
+    }
+    
+    
+    private String getPropertySuffix(String propertyName) throws ConfigurationPropertyStoreException {
+        return propSplit(propertyName, 1);
+    }
+    
+    /**
+     * <p>Splits a string into two parts: a prefix and a suffix.</p>
+     * <p>Prefix: Anything before the first dot "."</p>
+     * <p>Suffix: Anything after the first dot "."</p>
+     * 
+     * <p>Position specified as 0 or 1
+     * 
+     * @param str
+     * @param position
+     * @return dashes
+     */
+    private String propSplit(String str, int position) throws ConfigurationPropertyStoreException {
+        
+        String[] kvp = str.split("\\.", 2);
+        if (kvp.length <= 1) {
+            throw new ConfigurationPropertyStoreException("Invalid Property Format: " + str);
+        }
+        return kvp[position];
+    }
+    
+    /**
+     * <p>Checks for property validity (whether there is a prefix and a suffix, separated by a dot ".").</p>
+     * 
+     * @param key
+     * @return boolean
+     */
+    private boolean isValidProperty(String key) {
+        Pattern pattern = Pattern.compile("^([a-zA-Z0-9]+\\.){2,}[a-zA-Z0-9]+$");
+        Matcher matcher = pattern.matcher(key);
+        boolean matchFound = matcher.find();
+        return matchFound;
+    }
+    
+    /**
+     * <p>Utility to fetch a string with a specified number of dashes.</p>
+     * <p>Useful for table output in console.</p>
+     * 
+     * @param num
+     * @return dashes
+     */
+    private String getDashes(int num) {
+        String dashes = new String(new char [num]).replace("\0", "-");
+        return dashes;
+    }
+    
+    
+    /**
+     * <p>Retrieves the current/existing value for a specified property.</p>
+     * <p>This wrapper is required due to properties only being retrievable using both a prefix and a suffix.</p>
+     * 
+     * @param namespace
+     * @param key
+     * @return existingValue
+     * @throws ConfigurationPropertyStoreException 
+     */
+    private String getExistingValue(String namespace, String key) throws ConfigurationPropertyStoreException {
+     
+        String existingValue = 
+                namespaceCPS.get(namespace).getProperty(getPropertyPrefix(key), getPropertySuffix(key));
+                
+        return existingValue;
+        
     }
 }
