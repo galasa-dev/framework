f66dbdc768719bae575fff8df66ea050fb60d9a6
Michael Baylis <Michael.Baylis@uk.ibm.com>
2020-10-29 14:51:48 +0000

    Extend the DSS key access to include actions
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

---
+++ galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/spi/IDssAction.java
@@ -0,0 +1,16 @@
+/*
+ * Licensed Materials - Property of IBM
+ * 
+ * (c) Copyright IBM Corp. 2020.
+ */
+package dev.galasa.framework.spi;
+
+/**
+ * IDssAction is the basis for all updates to the Dynamic Status Store properties
+ * 
+ * @author Michael Baylis
+ *
+ */
+public interface IDssAction {
+    
+}
