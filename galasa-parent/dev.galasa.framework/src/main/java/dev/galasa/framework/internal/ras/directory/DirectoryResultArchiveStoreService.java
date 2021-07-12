28b1dfeaf06ab03ac7b7684ea6a645905b8c9ffb
Michael Baylis <Michael.Baylis@uk.ibm.com>
2021-07-12 13:30:45 +0100

    need the RAS changes as well so the cli can get it
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

--- galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/internal/ras/directory/DirectoryResultArchiveStoreService.java
+++ galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/internal/ras/directory/DirectoryResultArchiveStoreService.java
@@ -1,7 +1,7 @@
 /*
  * Licensed Materials - Property of IBM
  * 
- * (c) Copyright IBM Corp. 2019.
+ * (c) Copyright IBM Corp. 2019-2021.
  */
 package dev.galasa.framework.internal.ras.directory;
 
