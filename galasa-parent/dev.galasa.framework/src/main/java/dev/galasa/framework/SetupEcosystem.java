dbc55d638250a64cc451dd3ed8a08aad1b1b8f4e
Michael Baylis <Michael.Baylis@uk.ibm.com>
2022-05-16 09:49:49 +0100

    Initialise CPS
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

--- galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/SetupEcosystem.java
+++ galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/SetupEcosystem.java
@@ -10,6 +10,7 @@ import org.apache.commons.logging.LogFactory;
 import org.osgi.service.component.annotations.Component;
 
 import dev.galasa.framework.spi.FrameworkException;
+import dev.galasa.framework.spi.IConfigurationPropertyStoreService;
 import dev.galasa.framework.spi.IFramework;
 
 @Component(service = { SetupEcosystem.class })
@@ -39,6 +40,48 @@ public class SetupEcosystem {
         }
         
         framework = frameworkInitialisation.getFramework();
+        IConfigurationPropertyStoreService cps = framework.getConfigurationPropertyService("framework");
+        
+        // Check for System Environment variables to see what we need to set in the CPS
+        
+        // DSS value
+        
+        String dss = System.getenv("GALASA_EXTERNAL_DYNAMICSTATUS_STORE");
+        if (dss != null && !dss.trim().isEmpty()) {
+            dss = dss.trim();
+            
+            cps.setProperty("dynamicstatus.store", dss);
+            
+            logger.info("framework.dynamicstatus.store has been set to : " + dss);
+        } else {
+            logger.info("Not setting framework.dynamicstatus.store");
+        }
+        
+        // RAS value
+        
+        String ras = System.getenv("GALASA_EXTERNAL_RESULTARCHIVE_STORE");
+        if (ras != null && !ras.trim().isEmpty()) {
+            ras = ras.trim();
+            
+            cps.setProperty("resultarchive.store", dss);
+            
+            logger.info("framework.resultarchive.store has been set to : " + ras);
+        } else {
+            logger.info("Not setting framework.resultarchive.store");
+        }
+        
+        // CREDS value
+        
+        String creds = System.getenv("GALASA_EXTERNAL_CREDENTIALS_STORE");
+        if (creds != null && !creds.trim().isEmpty()) {
+            creds = creds.trim();
+            
+            cps.setProperty("credentials.store", creds);
+            
+            logger.info("framework.credentials.store has been set to : " + creds);
+        } else {
+            logger.info("Not setting framework.credentials.store");
+        }
                 
         logger.info("Ending Setup Ecosystem Service");
         
