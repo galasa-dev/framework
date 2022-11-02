f13896688d75ab51e1edca458cf4f4d8fd54177f
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2022-11-02 16:50:35 +0000

    java files to have correct copyright statement
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/BackupCPS.java
+++ galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/BackupCPS.java
@@ -1,9 +1,6 @@
 /*
- * Licensed Materials - Property of IBM
- * 
- * (c) Copyright IBM Corp. 2020.
+ * Copyright contributors to the Galasa project
  */
-
 package dev.galasa.framework;
 
 import java.io.IOException;
