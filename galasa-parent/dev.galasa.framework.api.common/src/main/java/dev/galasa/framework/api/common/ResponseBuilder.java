bd2bd0e89e9b3a3cd791bfbd96c3ef8bf7d307ed
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2023-06-26 17:33:51 +0100

    At least log a warning if we can't send back the HTTP response content
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- galasa-parent/dev.galasa.framework.api.common/src/main/java/dev/galasa/framework/api/common/ResponseBuilder.java
+++ galasa-parent/dev.galasa.framework.api.common/src/main/java/dev/galasa/framework/api/common/ResponseBuilder.java
@@ -6,17 +6,30 @@ package dev.galasa.framework.api.common;
 import java.io.PrintWriter;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class ResponseBuilder {
 
-    public HttpServletResponse buildResponse(HttpServletResponse resp, String contentType, String content, int status){
+	protected Log logger  =  LogFactory.getLog(ResponseBuilder.class);
+
+    public HttpServletResponse buildResponse(
+		HttpServletResponse resp, 
+		String contentType, 
+		String content, 
+		int status
+	) {
 		//Set headers for HTTP Response
 		resp.setStatus(status);
 		resp.setContentType(contentType);
 		resp.addHeader("Access-Control-Allow-Origin", "*");
+		
+		// Note: Auto-closes when we leave the code block
 		try (PrintWriter out = resp.getWriter()) {
 			out.print(content);
-		} catch (Exception e) {
-			// TODO : log the exception, saying we are going to ignore it.
+		} catch (Exception ex) {
+			// TODO: Should we actually ignore these exceptions ? How bad is this ?
+			logger.warn("Failed to build response object. Ignoring.", ex);
 		}
 		return resp;
 	}
