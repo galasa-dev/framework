4ea9da022348cab2386a50b5a9f21accc38171c7
Jade Carino <carino_jade@yahoo.co.uk>
2023-08-03 13:20:58 +0100

    More changes to the unit tests
    Co-authored-by: Eamonn Mansour <Eamonn.Mansour@ibm.com>
    Signed-off-by: Jade Carino <carino_jade@yahoo.co.uk>

--- galasa-parent/dev.galasa.framework.api.authentication/src/test/java/dev/galasa/framework/api/authentication/OidcProviderTest.java
+++ galasa-parent/dev.galasa.framework.api.authentication/src/test/java/dev/galasa/framework/api/authentication/OidcProviderTest.java
@@ -1,15 +1,21 @@
 package dev.galasa.framework.api.authentication;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
-import java.io.IOException;
+import java.net.http.HttpResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import com.google.gson.JsonObject;
 
-import dev.galasa.Test;
+import org.junit.Test;
 import dev.galasa.framework.api.authentication.internal.OidcProvider;
 
 public class OidcProviderTest {
+
+    private final Log logger = LogFactory.getLog(getClass());
 
     private JsonObject createMockRequestBody(String clientId, String secret, String refreshToken){
         JsonObject mockRequestBody = new JsonObject();
@@ -23,8 +29,15 @@ public class OidcProviderTest {
     public void testTokenPostWithValidRequestReturnsValidResponse() throws Exception {
         // Given...
         OidcProvider mockOidcProvider = mock(OidcProvider.class);
+        
+        HttpResponse<String> mockResponse = mock(HttpResponse.class);
+        when(mockResponse.body()).thenReturn("{ \"id_token\": \"this-is-a-jwt\" }");
 
-        JsonObject requestBody = createMockRequestBody("galasa", "abc", "???");
+        OidcProvider mockOidcProvider = mock(OidcProvider.class);
+        when(mockOidcProvider.sendTokenPost(any())).thenReturn(mockResponse);
+
+        JsonObject requestBody = createMockRequestBody("galasa", "abc", "thisisarefreshtoken");
+        
 
         // When...
         mockOidcProvider.sendTokenPost(requestBody);
@@ -32,6 +45,7 @@ public class OidcProviderTest {
 
 
         // Then...
+        assertTrue(true);
     }
 
 
