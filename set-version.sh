27269d9611096dae542e9d50633b26007bcf4770
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2023-10-19 15:55:31 +0100

    bump version up to 0.31.0
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- set-version.sh
+++ set-version.sh
@@ -165,3 +165,10 @@ cp $temp_dir/release.yaml ${BASEDIR}/release.yaml
 ## Update the openapi file
 cat ${BASEDIR}/openapi.yaml | sed "s/^[ ]*version[ ]*:.*/  version : \"$component_version\"/1" > $temp_dir/openapi.yaml
 cp $temp_dir/openapi.yaml ${BASEDIR}/openapi.yaml
+
+
+cat ${BASEDIR}/openapi.yaml | sed "s/^[ ]*version[ ]*:.*/  version : \"$component_version\"/1" > $temp_dir/openapi.yaml
+cp $temp_dir/openapi.yaml ${BASEDIR}/openapi.yaml
+
+cat ${BASEDIR}/test-api-locally.md | sed "s/^[ ]*export[ ]+GALASA_OBR_VERSION[ ]*=[ ]*.*$/export GALASA_OBR_VERSION=\"$component_version\"/1" > $temp_dir/test-api-locally.md 
+cp $temp_dir/test-api-locally.md  ${BASEDIR}/test-api-locally.md 
