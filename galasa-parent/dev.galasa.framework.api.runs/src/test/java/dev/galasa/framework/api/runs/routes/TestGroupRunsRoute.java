597fb1dbbdc3a6b69d8128415e951fc85ee4d02b
Savvas Kyriacou <savvasky@uk.ibm.com>
2023-10-24 15:00:50 +0100

    corrected regex on runs/{groupid} endpoint
    Signed-off-by: Savvas Kyriacou <savvasky@uk.ibm.com>

--- galasa-parent/dev.galasa.framework.api.runs/src/test/java/dev/galasa/framework/api/runs/routes/TestGroupRunsRoute.java
+++ galasa-parent/dev.galasa.framework.api.runs/src/test/java/dev/galasa/framework/api/runs/routes/TestGroupRunsRoute.java
@@ -194,6 +194,36 @@ public class TestGroupRunsRoute extends RunsServletTest {
         assertThat(outStream.toString()).isEqualTo(expectedJson);
     }
 
+
+    @Test
+    public void TestGetRunsWithUUIDGroupNameMultipleWithFinishedRunReturnsCompleteFalse() throws Exception {
+        // Given...
+		String groupName = "8149dc91-dabc-461a-b9e8-6f11a4455f59";
+        addRun("name1", "type1", "requestor1", "test1", "BUILDING","bundle1", "testClass1", groupName);
+        addRun("name2", "type2", "requestor2", "test2", "BUILDING","bundle2", "testClass2", groupName);
+        addRun("name3", "type3", "requestor3", "test3", "FINISHED","bundle3", "testClass3", groupName);
+        addRun("name4", "type4", "requestor4", "test4", "UP","bundle4", "testClass4", groupName);
+        addRun("name5", "type6", "requestor5", "test5", "DISCARDED","bundle5", "testClass6", groupName);
+        addRun("name6", "type6", "requestor6", "test6", "BUILDING","bundle6", "testClass6", groupName);
+        addRun("name7", "type7", "requestor7", "test7", "BUILDING","bundle7", "testClass7", groupName);
+        addRun("name8", "type8", "requestor8", "test8", "BUILDING","bundle8", "testClass8", groupName);
+        addRun("name9", "type9", "requestor9", "test9", "BUILDING","bundle9", "testClass9", groupName);
+        addRun("name10", "type10", "requestor10", "test10", "BUILDING","bundle10", "testClass10", groupName);
+        setServlet(groupName, groupName, this.runs);
+		MockRunsServlet servlet = getServlet();
+		HttpServletRequest req = getRequest();
+		HttpServletResponse resp = getResponse();
+        ServletOutputStream outStream = resp.getOutputStream();	
+
+        // When...
+        servlet.init();
+        servlet.doGet(req, resp);
+
+        // Then...
+        String expectedJson = generateExpectedJson(runs, "false");
+        assertThat(resp.getStatus()).isEqualTo(200);
+        assertThat(outStream.toString()).isEqualTo(expectedJson);
+    }
     /*
      * POST requests
      */
@@ -455,6 +485,29 @@ public class TestGroupRunsRoute extends RunsServletTest {
         assertThat(outStream.toString()).isEqualTo(expectedJson);
     }
 
+    @Test
+    public void TestPostUUIDGroupNameRunsWithValidBodyAndMultipleClassesReturnsOK() throws Exception {
+        // Given...
+		String groupName = "8149dc91-dabc-461a-b9e8-6f11a4455f59";
+        String[] classes = new String[]{"Class1/name", "Class2/name"};
+        String payload = generatePayload(classes, "requestorType", "user1", "this.test.stream", groupName, null);
+        
+        setServlet(groupName, groupName, payload, "POST");
+		MockRunsServlet servlet = getServlet();
+		HttpServletRequest req = getRequest();
+		HttpServletResponse resp = getResponse();
+        ServletOutputStream outStream = resp.getOutputStream();
+
+        // When...
+        servlet.init();
+        servlet.doPost(req, resp);
+
+        // Then...
+        String expectedJson = generateExpectedJson(runs, "false");
+        assertThat(resp.getStatus()).isEqualTo(201);
+        assertThat(outStream.toString()).isEqualTo(expectedJson);
+    }
+
     /*
      * Authorization Tests
      */
@@ -521,6 +574,29 @@ public class TestGroupRunsRoute extends RunsServletTest {
     public void TestPostRunsWithValidBodyAndMultipleClassesReturnsWithRequestorFromJWT() throws Exception {
         // Given...
 		String groupName = "valid";
+        String[] classes = new String[]{"Class1/name", "Class2/name"};
+        String payload = generatePayload(classes, "requestorType", "user1", "this.test.stream", groupName, "testRequestor");
+        
+        setServlet(groupName, groupName, payload, "POST", headerMap);
+		MockRunsServlet servlet = getServlet();
+		HttpServletRequest req = getRequest();
+		HttpServletResponse resp = getResponse();
+        ServletOutputStream outStream = resp.getOutputStream();
+
+        // When...
+        servlet.init();
+        servlet.doPost(req, resp);
+
+        // Then...
+        String expectedJson = generateExpectedJson(runs, "false");
+        assertThat(resp.getStatus()).isEqualTo(201);
+        assertThat(outStream.toString()).isEqualTo(expectedJson);
+    }
+
+    @Test
+    public void TestPostUUIDGroupNameRunsWithValidBodyAndMultipleClassesReturnsWithRequestorFromJWT() throws Exception {
+        // Given...
+		String groupName = "8149dc91-dabc-461a-b9e8-6f11a4455f59";
         String[] classes = new String[]{"Class1/name", "Class2/name"};
         String payload = generatePayload(classes, "requestorType", "user1", "this.test.stream", groupName, "testRequestor");
         
