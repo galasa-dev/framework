901ba09ef219d27a0c1ae8389765617e679af38b
Savvas Kyriacou <savvasky@uk.ibm.com>
2024-02-02 11:55:37 +0000

    updated to allow updates to GalasaGsonBuilder
    Signed-off-by: Savvas Kyriacou <savvasky@uk.ibm.com>

--- galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/spi/utils/GalasaGsonBuilder.java
+++ galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/spi/utils/GalasaGsonBuilder.java
@@ -5,6 +5,7 @@
  */
 package dev.galasa.framework.spi.utils;
 
+import java.lang.reflect.Type;
 import java.time.Instant;
 
 import com.google.gson.Gson;
@@ -12,15 +13,28 @@ import com.google.gson.GsonBuilder;
 
 public class GalasaGsonBuilder {
 
+    private GsonBuilder builder;
     /**
      * Creates a Gson builder to transform objects to JSON for use on the API endpoints.
      * HTMLEscaping is disabled as when it is enabled it will tranform special characters to their unicode character references
      * @return GsonBuilder
      */
-    public static Gson build() {
-        GsonBuilder builder = new GsonBuilder().disableHtmlEscaping();
-        builder.registerTypeAdapter(Instant.class, new GsonInstantTypeAdapater());
-        return builder.setPrettyPrinting().create();
+    public GalasaGsonBuilder () {
+        builder = new GsonBuilder()
+            .disableHtmlEscaping()
+            .registerTypeAdapter(Instant.class, new GsonInstantTypeAdapater())
+            .setPrettyPrinting();
+    }
+
+    public void registerTypeAdapter(Type type, Object object){
+        builder.registerTypeAdapter(type, object);
+    }
+    
+    public GsonBuilder getBuilder(){
+        return builder;
+    }
+    public Gson getGson() {
+        return builder.create();
     }
 
 }
