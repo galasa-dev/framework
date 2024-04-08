b2f096b8700cae539de389170b3035d443b16e7e
Savvas Kyriacou <savvasky@uk.ibm.com>
2024-04-08 15:50:27 +0100

    created BootstrapProperties class to use in both external and internal bootstrap endpoints
    Signed-off-by: Savvas Kyriacou <savvasky@uk.ibm.com>

--- galasa-parent/dev.galasa.framework.api.bootstrap/src/main/java/dev/galasa/framework/api/bootstrap/routes/BootstrapInternalRoute.java
+++ galasa-parent/dev.galasa.framework.api.bootstrap/src/main/java/dev/galasa/framework/api/bootstrap/routes/BootstrapInternalRoute.java
@@ -8,32 +8,29 @@ package dev.galasa.framework.api.bootstrap.routes;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Map;
 import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dev.galasa.framework.api.bootstrap.internal.BootstrapProperties;
 import dev.galasa.framework.api.common.BaseRoute;
 import dev.galasa.framework.api.common.QueryParameters;
 import dev.galasa.framework.api.common.ResponseBuilder;
-import dev.galasa.framework.spi.ConfigurationPropertyStoreException;
 import dev.galasa.framework.spi.FrameworkException;
 import dev.galasa.framework.spi.IFramework;
 
 public class BootstrapInternalRoute extends BaseRoute{
 
-    private IFramework               framework;  
-
-    private final Properties        configurationProperties = new Properties();
-    
     private final ArrayList<String> bootstrapKeys           = new ArrayList<>(Arrays.asList("framework.config.store",
             "framework.extra.bundles", "framework.testcatalog.url"));
 
+    private BootstrapProperties bootstrap;
+
     public BootstrapInternalRoute(ResponseBuilder responseBuilder, IFramework framework) {
         super(responseBuilder, "");
-        this.framework = framework;
+        this.bootstrap = new BootstrapProperties(framework, bootstrapKeys);
         logger.info("Galasa Bootstrap API activated");
     }
 
@@ -41,29 +38,10 @@ public class BootstrapInternalRoute extends BaseRoute{
     public HttpServletResponse handleGetRequest(String pathInfo, QueryParameters queryParams, 
             HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException, FrameworkException {
-        getLatestPropertiesFromFramework();
-        Properties actualBootstrap = new Properties();
-        synchronized (this.configurationProperties) {
-            actualBootstrap.putAll(this.configurationProperties);
-        }
+        Properties actualBootstrap = bootstrap.getProperties();
         actualBootstrap.store(response.getWriter(), "Galasa Bootstrap Properties");
         response = getResponseBuilder().buildResponseHeaders(response, "text/plain", HttpServletResponse.SC_OK);
         return response;
-    }
-
-
-    private void getLatestPropertiesFromFramework() throws ConfigurationPropertyStoreException {
-        Map<String,String> properties = framework.getConfigurationPropertyService("bootstrap").getAllProperties();
-        synchronized (configurationProperties) {
-            for (String key : bootstrapKeys) {
-                String value = (String) properties.get(key);
-                if (value != null) {
-                    this.configurationProperties.put(key, value);
-                } else {
-                    this.configurationProperties.remove(key);
-                }
-            }
-        }
     }
 
 }
