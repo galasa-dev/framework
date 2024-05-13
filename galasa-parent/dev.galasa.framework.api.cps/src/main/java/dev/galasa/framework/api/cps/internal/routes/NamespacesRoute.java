35bfe5f899421d4abea86b90c2e29a4c33845640
Savvas Kyriacou <savvasky@uk.ibm.com>
2024-05-13 15:10:10 +0100

    corrected pattern for /cps endpoint and added unit tests
    Signed-off-by: Savvas Kyriacou <savvasky@uk.ibm.com>

--- galasa-parent/dev.galasa.framework.api.cps/src/main/java/dev/galasa/framework/api/cps/internal/routes/NamespacesRoute.java
+++ galasa-parent/dev.galasa.framework.api.cps/src/main/java/dev/galasa/framework/api/cps/internal/routes/NamespacesRoute.java
@@ -34,7 +34,7 @@ import dev.galasa.framework.spi.utils.GalasaGson;
  */
 public class NamespacesRoute extends CPSRoute {
 
-    protected static final String path = "\\/";
+    protected static final String path = "\\/?";
     private static final GalasaGson gson = new GalasaGson();
 
 
