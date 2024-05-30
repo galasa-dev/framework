285b1507e1c6d908950adfa111650acdeff2e892
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2024-05-30 16:16:54 +0100

    Add token creation to POST /auth/tokens (#559)
    * Move token creation into /auth/tokens route, allow POST /auth/tokens through JWT filter
    
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
    
    * Add test to verify GET /auth/tokens still needs a JWT
    
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
    
    * Add client API version param to auth routes
    
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
    
    * Add HttpMethod enum
    
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>

--- galasa-parent/dev.galasa.framework.api.authentication/src/test/java/dev/galasa/framework/api/authentication/routes/AuthTokensRouteTest.java
+++ galasa-parent/dev.galasa.framework.api.authentication/src/test/java/dev/galasa/framework/api/authentication/routes/AuthTokensRouteTest.java
@@ -7,8 +7,8 @@ package dev.galasa.framework.api.authentication.routes;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.net.http.HttpResponse;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -17,24 +17,66 @@ import javax.servlet.ServletOutputStream;
 
 import org.junit.Test;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import dev.galasa.framework.api.authentication.internal.DexGrpcClient;
 import dev.galasa.framework.api.authentication.internal.routes.AuthTokensRoute;
 import dev.galasa.framework.api.authentication.mocks.MockAuthenticationServlet;
+import dev.galasa.framework.api.authentication.mocks.MockDexGrpcClient;
+import dev.galasa.framework.api.authentication.mocks.MockOidcProvider;
 import dev.galasa.framework.api.common.AuthToken;
 import dev.galasa.framework.api.common.BaseServletTest;
 import dev.galasa.framework.api.common.mocks.MockHttpServletRequest;
 import dev.galasa.framework.api.common.mocks.MockHttpServletResponse;
+import dev.galasa.framework.api.common.mocks.MockTimeService;
 import dev.galasa.framework.api.common.mocks.MockAuthStoreService;
 import dev.galasa.framework.spi.auth.IAuthToken;
 import dev.galasa.framework.spi.auth.User;
 import dev.galasa.framework.spi.utils.GalasaGson;
 import dev.galasa.framework.api.common.mocks.MockFramework;
+import dev.galasa.framework.api.common.mocks.MockHttpResponse;
 
 public class AuthTokensRouteTest extends BaseServletTest {
 
     private static final GalasaGson gson = new GalasaGson();
+
+    private String buildRequestPayload(String clientId, String refreshToken, String authCode) {
+        return buildRequestPayload(clientId, refreshToken, authCode, null);
+    }
+
+    private String buildRequestPayload(String clientId, String refreshToken, String authCode, String description) {
+        JsonObject requestPayload = new JsonObject();
+        if (clientId != null) {
+            requestPayload.addProperty("client_id", clientId);
+        }
+
+        if (refreshToken != null) {
+            requestPayload.addProperty("refresh_token", refreshToken);
+        }
+
+        if (authCode != null) {
+            requestPayload.addProperty("code", authCode);
+        }
+
+        if (description != null) {
+            requestPayload.addProperty("description", description);
+        }
+
+        String requestPayloadStr = gson.toJson(requestPayload);
+        return requestPayloadStr;
+    }
+
+    private String buildTokenResponse(String jwt, String refreshToken) {
+        JsonObject responseJson = new JsonObject();
+
+        responseJson.addProperty("id_token", jwt);
+        responseJson.addProperty("refresh_token", refreshToken);
+
+        return gson.toJson(responseJson);
+    }
 
     /**
      * Compares a list of expected authentication tokens against a JSON array
@@ -58,8 +100,7 @@ public class AuthTokensRouteTest extends BaseServletTest {
     @Test
     public void testAuthTokensRouteRegexMatchesOnlyTokens(){
         //Given...
-        MockAuthStoreService authStoreService = new MockAuthStoreService(new ArrayList<>());
-        String tokensRoutePath = new AuthTokensRoute(null, authStoreService).getPath();
+        String tokensRoutePath = new AuthTokensRoute(null, null, null, null, null).getPath();
 
         //When...
         Pattern tokensRoutePattern = Pattern.compile(tokensRoutePath);
@@ -183,4 +224,340 @@ public class AuthTokensRouteTest extends BaseServletTest {
         assertThat(servletResponse.getHeader("Access-Control-Allow-Origin")).isEqualTo("*");
         checkOrderMatches(expectedTokenOrder, getJsonArrayFromJson(output, "tokens"));
     }
+
+    @Test
+    public void testAuthPostRequestWithEmptyRequestPayloadReturnsBadRequestError() throws Exception {
+        // Given...
+        MockAuthenticationServlet servlet = new MockAuthenticationServlet();
+
+        MockHttpServletRequest mockRequest = new MockHttpServletRequest(null, "", "POST");
+        MockHttpServletResponse servletResponse = new MockHttpServletResponse();
+        ServletOutputStream outStream = servletResponse.getOutputStream();
+
+        // When...
+        servlet.init();
+        servlet.doPost(mockRequest, servletResponse);
+
+        // Then...
+        // Expecting this json:
+        // {
+        // "error_code" : 5400,
+        // "error_message" : "GAL5400E: Error occured when trying to execute request
+        // '/auth'. Please check your request parameters or report the problem to your
+        // Galasa Ecosystem owner."
+        // }
+        assertThat(servletResponse.getStatus()).isEqualTo(400);
+        checkErrorStructure(outStream.toString(), 5400, "GAL5400E");
+    }
+
+    @Test
+    public void testAuthPostRequestWithInvalidRequestPayloadReturnsBadRequestError() throws Exception {
+        // Given...
+        MockAuthenticationServlet servlet = new MockAuthenticationServlet();
+
+        // Payload with a missing "refresh_token" field
+        String payload = buildRequestPayload("dummy-id", null, null);
+        MockHttpServletRequest mockRequest = new MockHttpServletRequest("/tokens", payload, "POST");
+        MockHttpServletResponse servletResponse = new MockHttpServletResponse();
+        ServletOutputStream outStream = servletResponse.getOutputStream();
+
+        // When...
+        servlet.init();
+        servlet.doPost(mockRequest, servletResponse);
+
+        // Then...
+        // Expecting this json:
+        // {
+        // "error_code" : 5400,
+        // "error_message" : "GAL5400E: Error occured when trying to execute request
+        // '/auth'. Please check your request parameters or report the problem to your
+        // Galasa Ecosystem owner."
+        // }
+        assertThat(servletResponse.getStatus()).isEqualTo(400);
+        checkErrorStructure(outStream.toString(), 5400, "GAL5400E");
+    }
+
+    @Test
+    public void testAuthPostRequestWithValidRefreshTokenRequestPayloadReturnsJWT() throws Exception {
+        // Given...
+        String dummyJwt = "this-is-a-jwt";
+        String dummyRefreshToken = "this-is-a-refresh-token";
+        String mockResponseJson = buildTokenResponse(dummyJwt, dummyRefreshToken);
+
+        HttpResponse<String> mockResponse = new MockHttpResponse<String>(mockResponseJson);
+
+        String clientId = "dummy-id";
+        String clientSecret = "asecret";
+        String refreshToken = "here-is-a-token";
+
+        MockOidcProvider mockOidcProvider = new MockOidcProvider(mockResponse);
+
+        DexGrpcClient mockDexGrpcClient = new MockDexGrpcClient("http://issuer", clientId, clientSecret, "http://callback");
+
+        MockTimeService mockTimeService = new MockTimeService(Instant.now());
+        MockAuthStoreService mockAuthStoreService = new MockAuthStoreService(mockTimeService);
+        MockFramework mockFramework = new MockFramework(mockAuthStoreService);
+
+        MockAuthenticationServlet servlet = new MockAuthenticationServlet(mockOidcProvider, mockDexGrpcClient, mockFramework);
+
+        String payload = buildRequestPayload(clientId, refreshToken, null);
+        MockHttpServletRequest mockRequest = new MockHttpServletRequest("/tokens", payload, "POST");
+        MockHttpServletResponse servletResponse = new MockHttpServletResponse();
+        ServletOutputStream outStream = servletResponse.getOutputStream();
+
+        // When...
+        servlet.init();
+        servlet.doPost(mockRequest, servletResponse);
+
+        // Then...
+        // Expecting this json:
+        // {
+        //   "jwt": "this-is-a-jwt",
+        //   "refresh_token": "this-is-a-refresh-token",
+        // }
+        JsonObject expectedJsonObject = new JsonObject();
+        expectedJsonObject.addProperty("jwt", dummyJwt);
+        expectedJsonObject.addProperty("refresh_token", dummyRefreshToken);
+
+        assertThat(servletResponse.getStatus()).isEqualTo(200);
+        assertThat(outStream.toString()).isEqualTo(gson.toJson(expectedJsonObject));
+        assertThat(mockAuthStoreService.getTokens()).isEmpty();
+    }
+
+    @Test
+    public void testAuthPostRequestWithDescriptionInRequestReturnsJWTAndStoresTokenInAuthStore() throws Exception {
+        // Given...
+        String tokenDescription = "my new galasactl token for my macOS laptop";
+        String userName = "JohnDoe";
+
+        Algorithm mockJwtSigningAlgorithm = Algorithm.HMAC256("dummysecret");
+        String dummyJwt = JWT.create()
+            .withSubject("validUserId")
+            .withIssuedAt(Instant.EPOCH)
+            .withClaim("name", userName)
+            .sign(mockJwtSigningAlgorithm);
+
+        String dummyRefreshToken = "this-is-a-refresh-token";
+        String mockResponseJson = buildTokenResponse(dummyJwt, dummyRefreshToken);
+
+        HttpResponse<String> mockResponse = new MockHttpResponse<String>(mockResponseJson);
+
+        String clientId = "dummy-id";
+        String clientSecret = "asecret";
+        String authCode = "thisisacode";
+
+        String callbackUri = "http://api.host/auth/callback";
+        String issuerUrl = "http://dummy.host";
+
+        MockOidcProvider mockOidcProvider = new MockOidcProvider(mockResponse);
+        DexGrpcClient mockDexGrpcClient = new MockDexGrpcClient(issuerUrl, clientId, clientSecret, callbackUri);
+
+        Instant tokenCreationTime = Instant.now();
+        MockTimeService mockTimeService = new MockTimeService(tokenCreationTime);
+        MockAuthStoreService mockAuthStoreService = new MockAuthStoreService(mockTimeService);
+        MockFramework mockFramework = new MockFramework(mockAuthStoreService);
+
+        MockAuthenticationServlet servlet = new MockAuthenticationServlet(mockOidcProvider, mockDexGrpcClient, mockFramework);
+
+        String payload = buildRequestPayload(clientId, null, authCode, tokenDescription);
+        MockHttpServletRequest mockRequest = new MockHttpServletRequest("/tokens", payload, "POST");
+        MockHttpServletResponse servletResponse = new MockHttpServletResponse();
+        ServletOutputStream outStream = servletResponse.getOutputStream();
+
+        // When...
+        servlet.init();
+        servlet.doPost(mockRequest, servletResponse);
+
+        // Then...
+        // Expecting this json:
+        // {
+        //   "jwt": "ey....",
+        //   "refresh_token": "this-is-a-refresh-token",
+        // }
+        JsonObject expectedJsonObject = new JsonObject();
+        expectedJsonObject.addProperty("jwt", dummyJwt);
+        expectedJsonObject.addProperty("refresh_token", dummyRefreshToken);
+
+        assertThat(servletResponse.getStatus()).isEqualTo(200);
+        assertThat(outStream.toString()).isEqualTo(gson.toJson(expectedJsonObject));
+
+        // A record of the newly-created token should have been added to the auth store
+        List<IAuthToken> tokens = mockAuthStoreService.getTokens();
+        assertThat(tokens).hasSize(1);
+
+        IAuthToken newToken = tokens.get(0);
+        assertThat(newToken.getDescription()).isEqualTo(tokenDescription);
+        assertThat(newToken.getCreationTime()).isEqualTo(tokenCreationTime);
+        assertThat(newToken.getOwner().getLoginId()).isEqualTo(userName);
+    }
+
+    @Test
+    public void testAuthPostRequestWithValidAuthCodeRequestPayloadReturnsJWT() throws Exception {
+        // Given...
+        String dummyJwt = "this-is-a-jwt";
+        String dummyRefreshToken = "this-is-a-refresh-token";
+        String mockResponseJson = buildTokenResponse(dummyJwt, dummyRefreshToken);
+
+        HttpResponse<String> mockResponse = new MockHttpResponse<String>(mockResponseJson);
+
+        String clientId = "dummy-id";
+        String clientSecret = "asecret";
+        String authCode = "thisisacode";
+
+        String callbackUri = "http://api.host/auth/callback";
+        String issuerUrl = "http://dummy.host";
+
+        MockOidcProvider mockOidcProvider = new MockOidcProvider(mockResponse);
+
+        DexGrpcClient mockDexGrpcClient = new MockDexGrpcClient(issuerUrl, clientId, clientSecret, callbackUri);
+
+        Instant tokenCreationTime = Instant.now();
+        MockTimeService mockTimeService = new MockTimeService(tokenCreationTime);
+        MockAuthStoreService mockAuthStoreService = new MockAuthStoreService(mockTimeService);
+        MockFramework mockFramework = new MockFramework(mockAuthStoreService);
+
+        MockAuthenticationServlet servlet = new MockAuthenticationServlet(mockOidcProvider, mockDexGrpcClient, mockFramework);
+
+        String payload = buildRequestPayload(clientId, null, authCode);
+        MockHttpServletRequest mockRequest = new MockHttpServletRequest("/tokens", payload, "POST");
+        MockHttpServletResponse servletResponse = new MockHttpServletResponse();
+        ServletOutputStream outStream = servletResponse.getOutputStream();
+
+        // When...
+        servlet.init();
+        servlet.doPost(mockRequest, servletResponse);
+
+        // Then...
+        // Expecting this json:
+        // {
+        //   "jwt": "this-is-a-jwt",
+        //   "refresh_token": "this-is-a-refresh-token",
+        // }
+        JsonObject expectedJsonObject = new JsonObject();
+        expectedJsonObject.addProperty("jwt", dummyJwt);
+        expectedJsonObject.addProperty("refresh_token", dummyRefreshToken);
+
+        assertThat(servletResponse.getStatus()).isEqualTo(200);
+        assertThat(outStream.toString()).isEqualTo(gson.toJson(expectedJsonObject));
+
+        // No new token records should have been added to the auth store since no token description was provided
+        assertThat(mockAuthStoreService.getTokens()).isEmpty();
+    }
+
+    @Test
+    public void testAuthPostRequestWithFailingAuthStoreReturnsError() throws Exception {
+        // Given...
+        String tokenDescription = "my new galasactl token for my macOS laptop";
+        String userName = "JohnDoe";
+
+        Algorithm mockJwtSigningAlgorithm = Algorithm.HMAC256("dummysecret");
+        String dummyJwt = JWT.create()
+            .withSubject("validUserId")
+            .withIssuedAt(Instant.EPOCH)
+            .withClaim("name", userName)
+            .sign(mockJwtSigningAlgorithm);
+
+        String dummyRefreshToken = "this-is-a-refresh-token";
+        String mockResponseJson = buildTokenResponse(dummyJwt, dummyRefreshToken);
+
+        HttpResponse<String> mockResponse = new MockHttpResponse<String>(mockResponseJson);
+
+        String clientId = "dummy-id";
+        String clientSecret = "asecret";
+        String authCode = "thisisacode";
+
+        String callbackUri = "http://api.host/auth/callback";
+        String issuerUrl = "http://dummy.host";
+
+        MockOidcProvider mockOidcProvider = new MockOidcProvider(mockResponse);
+
+        DexGrpcClient mockDexGrpcClient = new MockDexGrpcClient(issuerUrl, clientId, clientSecret, callbackUri);
+
+        Instant tokenCreationTime = Instant.now();
+        MockTimeService mockTimeService = new MockTimeService(tokenCreationTime);
+        MockAuthStoreService mockAuthStoreService = new MockAuthStoreService(mockTimeService);
+        mockAuthStoreService.setThrowException(true);
+
+        MockFramework mockFramework = new MockFramework(mockAuthStoreService);
+
+        MockAuthenticationServlet servlet = new MockAuthenticationServlet(mockOidcProvider, mockDexGrpcClient, mockFramework);
+
+        String payload = buildRequestPayload(clientId, null, authCode, tokenDescription);
+        MockHttpServletRequest mockRequest = new MockHttpServletRequest("/tokens", payload, "POST");
+        MockHttpServletResponse servletResponse = new MockHttpServletResponse();
+        ServletOutputStream outStream = servletResponse.getOutputStream();
+
+        // When...
+        servlet.init();
+        servlet.doPost(mockRequest, servletResponse);
+
+        // Then...
+        assertThat(servletResponse.getStatus()).isEqualTo(500);
+        checkErrorStructure(outStream.toString(), 5056, "GAL5056E", "Internal server error occurred when storing the new Galasa token with description", tokenDescription);
+    }
+
+    @Test
+    public void testAuthPostRequestThrowsUnexpectedErrorReturnsServerError() throws Exception {
+        // Given...
+        MockOidcProvider mockOidcProvider = new MockOidcProvider();
+        mockOidcProvider.setThrowException(true);
+
+        String clientId = "myclient";
+        MockDexGrpcClient mockDexGrpcClient = new MockDexGrpcClient("http://issuer", clientId, "secret", "http://callback");
+
+        MockAuthenticationServlet servlet = new MockAuthenticationServlet(mockOidcProvider, mockDexGrpcClient);
+
+        String payload = buildRequestPayload("dummy-id", "here-is-a-token", null);
+        MockHttpServletRequest mockRequest = new MockHttpServletRequest("/tokens", payload, "POST");
+        MockHttpServletResponse servletResponse = new MockHttpServletResponse();
+        ServletOutputStream outStream = servletResponse.getOutputStream();
+
+        // When...
+        servlet.init();
+        servlet.doPost(mockRequest, servletResponse);
+
+        // Then...
+        // Expecting this json:
+        // {
+        // "error_code" : 5000,
+        // "error_message" : "GAL5000E: Error occured when trying to access the
+        // endpoint. Report the problem to your Galasa Ecosystem owner."
+        // }
+        assertThat(servletResponse.getStatus()).isEqualTo(500);
+        checkErrorStructure(outStream.toString(), 5000, "GAL5000E", "Error occured when trying to access the endpoint");
+    }
+
+    @Test
+    public void testAuthPostRequestWithTokenErrorResponseReturnsServerError() throws Exception {
+        // Given...
+        JsonObject dummyErrorJson = new JsonObject();
+        dummyErrorJson.addProperty("error", "oh no, something went wrong!");
+        HttpResponse<String> mockResponse = new MockHttpResponse<String>(gson.toJson(dummyErrorJson));
+
+        MockOidcProvider mockOidcProvider = new MockOidcProvider(mockResponse);
+
+        String clientId = "myclient";
+        MockDexGrpcClient mockDexGrpcClient = new MockDexGrpcClient("http://issuer", clientId, "secret", "http://callback");
+
+        MockAuthenticationServlet servlet = new MockAuthenticationServlet(mockOidcProvider, mockDexGrpcClient);
+
+        String payload = buildRequestPayload("dummy-id", "here-is-a-token", null);
+        MockHttpServletRequest mockRequest = new MockHttpServletRequest("/tokens", payload, "POST");
+        MockHttpServletResponse servletResponse = new MockHttpServletResponse();
+        ServletOutputStream outStream = servletResponse.getOutputStream();
+
+        // When...
+        servlet.init();
+        servlet.doPost(mockRequest, servletResponse);
+
+        // Then...
+        // Expecting this json:
+        // {
+        //   "error_code": 5055,
+        //   "error_message": "GAL5055E: ..."
+        // }
+        assertThat(servletResponse.getStatus()).isEqualTo(500);
+        checkErrorStructure(outStream.toString(), 5055, "GAL5055E", "Failed to get a JWT and a refresh token from the Galasa Dex server", "The Dex server did not respond with a JWT and refresh token");
+    }
+
 }
