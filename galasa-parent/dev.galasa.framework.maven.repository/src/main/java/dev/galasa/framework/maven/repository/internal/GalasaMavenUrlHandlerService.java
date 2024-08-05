9b9c34c1a5324efe6461962f5cd967e64a979942
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2024-08-05 11:27:05 +0100

    1938 - Removed unused inport
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- galasa-parent/dev.galasa.framework.maven.repository/src/main/java/dev/galasa/framework/maven/repository/internal/GalasaMavenUrlHandlerService.java
+++ galasa-parent/dev.galasa.framework.maven.repository/src/main/java/dev/galasa/framework/maven/repository/internal/GalasaMavenUrlHandlerService.java
@@ -7,7 +7,6 @@ package dev.galasa.framework.maven.repository.internal;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
