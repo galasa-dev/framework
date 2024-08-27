82b4ba45f4ebb44ea78d24974d6c39b91a6515da
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2024-08-28 00:09:04 +0100

    correct bad syntax in bundle manager.
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/internal/runner/BundleManager.java
+++ galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/internal/runner/BundleManager.java
@@ -5,19 +5,14 @@
  */
 package dev.galasa.framework.internal.runner;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.felix.bundlerepository.RepositoryAdmin;
 import org.osgi.framework.BundleContext;
 
-import dev.galasa.framework.AbstractTestRunner;
 import dev.galasa.framework.BundleManagement;
 import dev.galasa.framework.IBundleManager;
 import dev.galasa.framework.spi.FrameworkException;
 
 public class BundleManager implements IBundleManager {
-
-    private Log logger = LogFactory.getLog(BundleManager.class);
 
     @Override
     public boolean isBundleActive(BundleContext bundleContext, String bundleSymbolicName) {
@@ -33,9 +28,7 @@ public class BundleManager implements IBundleManager {
     @Override
     public void loadBundle(RepositoryAdmin repositoryAdmin, BundleContext bundleContext, String bundleSymbolicName)
             throws FrameworkException {
-        try {
-            BundleManagement.loadBundle(repositoryAdmin, bundleContext, bundleSymbolicName);
-        }
+        BundleManagement.loadBundle(repositoryAdmin, bundleContext, bundleSymbolicName);
     }
     
 }
