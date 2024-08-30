3d4b70b3f63e169d8031707450e4b0971d3578cb
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2024-08-30 11:17:43 +0100

    run-locally.sh now picks up the framework version
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- run-locally.sh
+++ run-locally.sh
@@ -120,6 +120,11 @@ info "Boot jar is at ${BOOT_FOLDER}/${boot_jar_name}"
 
 # Work out where the locally-build-OBR is held...
 OBR_VERSION="0.31.0"
+# version = "0.37.0"
+
+framework_version=$(cat $BASEDIR/galasa-parent/dev.galasa.framework/build.gradle | grep "version =" | cut -f2 -d'"' | xargs)
+OBR_VERSION="$framework_version"
+info "Using OBR version $OBR_VERSION"
 
 M2_PATH=~/.m2
 
